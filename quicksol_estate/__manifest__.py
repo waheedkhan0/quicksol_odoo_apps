4cc2bb66cfe85d6ef75b4ffc7b15742d24d849d0
waheed.khan@phraise.io <waheedkhan05@gmail.com>
2024-10-12 05:54:00 +0500

    images banner and description for odoo store

--- quicksol_estate/__manifest__.py
+++ quicksol_estate/__manifest__.py
@@ -4,21 +4,16 @@
     'category': 'Real Estate',
     'summary': 'Manage properties, sales, leases, tenants, and agents',
     'description': """
-### Real Estate Management Module
+Real Estate Management Module
 This module allows real estate agencies, property managers, and landlords to manage their properties, agents, tenants, and leases easily.
 
-#### Key Features:
-- **Property Management**: Add and manage properties with detailed information like size, number of rooms, amenities, and condition.
-- **Image Gallery**: Upload multiple images for each property.
-- **Agent and Tenant Management**: Assign agents and tenants to properties and track their information.
-- **Lease Management**: Handle leasing contracts and track rental properties.
-- **Property Status**: Set the status of a property (available, sold, rented, etc.).
+Key Features:
+- Property Management**: Add and manage properties with detailed information like size, number of rooms, amenities, and condition.
+- Image Gallery**: Upload multiple images for each property.
+- Agent and Tenant Management**: Assign agents and tenants to properties and track their information.
+- Lease Management**: Handle leasing contracts and track rental properties.
+- Property Status**: Set the status of a property (available, sold, rented, etc.).
 
-#### Screenshots:
-![Property List View](static/description/screenshot_property_list.png)
-![Property Detail View](static/description/screenshot_property_detail.png)
-![Agent Detail View](static/description/screenshot_agent_profile.png)
-![Agent Detail View](static/description/screenshot_tenant_list.png)
 """,
     'author': 'Quicksol Technolgies',
     'website': 'https://quicksol.ca',
@@ -35,6 +30,7 @@ This module allows real estate agencies, property managers, and landlords to man
         'views/real_estate_menus.xml',
     ],
 '   license': 'LGPL-3',
+'images': ['static/description/banner.png'],
     'icon': 'icon.png',
     'installable': True,
     'auto_install' : False,
